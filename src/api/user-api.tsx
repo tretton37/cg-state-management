d5cb4d12456f4783a35671cef90ff9767c1419b4
David Preutz <david.preutz@1337.tech>
2022-10-28 10:04:44 +0200

    Support json serialization

--- src/api/user-api.tsx
+++ src/api/user-api.tsx
@@ -1,5 +1,5 @@
 export const GetUserById = (id: number): Promise<IUser> => {
-  const user = userData.find((user) => user.id === id);
+  const user = userData.find((user) => user.id === id.toString());
 
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -30,23 +30,23 @@ const SaveUser = (user: IUserModel): Promise<boolean> => {
 const userData: IUserModel[] = [
   {
     name: 'David',
-    id: 1,
-    birthDate: new Date('1979-01-01'),
+    id: '1',
+    birthDate: new Date('1979-01-01').toISOString(),
   },
   {
     name: 'Victor',
-    id: 2,
-    birthDate: new Date('1990-01-07'),
+    id: '2',
+    birthDate: new Date('1990-01-07').toISOString(),
   },
   {
     name: 'Christian',
-    id: 3,
-    birthDate: new Date('1990-06-07'),
+    id: '3',
+    birthDate: new Date('1990-06-07').toISOString(),
   },
   {
     name: 'Joakim',
-    id: 4,
-    birthDate: new Date('1982'),
+    id: '4',
+    birthDate: new Date('1982').toISOString(),
   },
 ];
 
@@ -57,27 +57,39 @@ export class User implements IUser {
 
   constructor(model: IUserModel) {
     this.name = model.name;
-    this.id = model.id;
-    this.birthDate = model.birthDate;
+    this.id = Number(model.id);
+    this.birthDate = new Date(model.birthDate);
   }
 
   public Save = () => {
-    return SaveUser(this);
+    return SaveUser(this.toJson());
   };
 
   public Hydrate = (model: IUserModel) => {
     this.name = model.name;
-    this.id = model.id;
-    this.birthDate = model.birthDate;
+    this.id = Number(model.id);
+    this.birthDate = new Date(model.birthDate);
+  };
+
+  public toJson = (): IUserModel => {
+    return {
+      name: this.name,
+      id: this.id.toString(),
+      birthDate: this.birthDate.toString(),
+    };
   };
 }
 
-export interface IUser extends IUserModel {
+export interface IUser {
   Save: () => Promise<boolean>;
+  Hydrate: (model: IUserModel) => void;
+  name: string;
+  id: number;
+  birthDate: Date;
 }
 
 export interface IUserModel {
   name: string;
-  id: number;
-  birthDate: Date;
+  id: string;
+  birthDate: string;
 }
