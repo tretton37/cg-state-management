a497a2976bbfb7891837ea5373b5304b8ebd640b
Andre Brdoch <andrebrdoch@yhahoo.de>
2023-01-18 16:54:09 +0100

    feat: Save user state on save

--- src/examples/jotai/JotaiMain.tsx
+++ src/examples/jotai/JotaiMain.tsx
@@ -3,14 +3,24 @@ import { EditUser } from '../../app/components/edit-user/EditUser';
 import { SelectCustomTheme } from '../../app/components/SelectCustomTheme';
 import { UserList } from '../../app/components/user-list/UserList';
 import { H2 } from '../../ui/styles';
+import { atomUser } from './atoms';
+import { useAtom } from 'jotai';
+import { SaveUser } from '../../api/user-api';
 
 export const JotaiMain: React.FC = () => {
+  const [, setUser] = useAtom(atomUser);
   return (
     <Wrapper>
       <H2>Jotai</H2>
       <EditUser
         statemanager="jotai"
-        saveUserHandler={(_) => Promise.resolve(true)}
+        saveUserHandler={async (user) => {
+          setUser(user);
+          // eslint-disable-next-line
+          // @ts-ignore
+          await SaveUser({ user });
+          return true;
+        }}
       />
       <UserList users={[]} />
 
