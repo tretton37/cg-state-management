1c69d2707d4f5baae93483fd6dbddfcd9ffb90db
Andre Brdoch <andrebrdoch@yhahoo.de>
2023-01-19 08:24:14 +0100

    feat: Persist theme

--- src/examples/jotai/atoms.ts
+++ src/examples/jotai/atoms.ts
@@ -1,6 +1,7 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 import { IUser } from '../../api/types';
 
 export const atomUser = atom<IUser | undefined>(undefined);
 
-export const atomTheme = atom('red');
+export const atomTheme = atomWithStorage('jotai-theme', 'red');
